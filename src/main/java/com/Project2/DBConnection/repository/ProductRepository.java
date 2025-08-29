1c03012f812b963aaa3eae907e80472e7cde41f0
Hariharan4753 <s.hariharan220@gmail.com>
2025-08-29 22:29:04 +0530

    update

--- src/main/java/com/Project2/DBConnection/repository/ProductRepository.java
+++ src/main/java/com/Project2/DBConnection/repository/ProductRepository.java
@@ -1,4 +1,4 @@
-package com.Project2.DBConnection.Repository; // NEW: lowercase, conventional package name
+package com.Project2.DBConnection.repository; 
 
 import com.Project2.DBConnection.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,12 +16,9 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // NEW: Find products containing keyword in name (case-insensitive)
     List<Product> findByNameContainingIgnoreCase(String keyword);
 
-    // NEW: Find products within a price range
     List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 
-    // NEW: Find all products with stock less than a certain amount
     List<Product> findByQuantityLessThan(Integer threshold);
 }
